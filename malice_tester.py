9f04d06fc6edc00ecb6620a21615e44041b76532
Hermitao <vkriegergranemann@gmail.com>
2023-08-17 01:31:55 -0300

    Add tests notebook

--- malice_tester.py
+++ malice_tester.py
@@ -1,4 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
@@ -16,18 +17,6 @@ log_file = f'logs/log {current_time}.txt'
 logging.basicConfig(filename=log_file, level=logging.DEBUG, encoding='utf-8')
 
 def cos_sim(sentence1_emb, sentence2_emb):
-    """
-    Cosine similarity between two columns of sentence embeddings
-
-    Args:
-      sentence1_emb: sentence1 embedding column
-      sentence2_emb: sentence2 embedding column
-
-    Returns:
-      The row-wise cosine similarity between the two columns.
-      For instance is sentence1_emb=[a,b,c] and sentence2_emb=[x,y,z]
-      Then the result is [cosine_similarity(a,x), cosine_similarity(b,y), cosine_similarity(c,z)]
-    """
     cos_sim = cosine_similarity(sentence1_emb, sentence2_emb)
     return np.diag(cos_sim)
 
