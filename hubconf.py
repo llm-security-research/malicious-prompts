2b0432252cdb61ab44e912b06709959291092e67
Hermitao <vkriegergranemann@gmail.com>
2024-03-02 22:15:25 -0300

    Update hubconf.py file

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,10 @@
 import torch
+import os
 dependencies = ['torch']
 
 def promptsentinel_unbalanced_v1(*args, **kwargs):
-    model = torch.load("./PromptSentinel/PromptSentinel-Unbalanced-v1/model.pth")
+    # model = torch.load("./PromptSentinel/PromptSentinel-Unbalanced-v1/model.pth")
+    model_path = os.path.join(os.path.dirname(__file__), 'PromptSentinel/PromptSentinel-Unbalanced-v1/model.pth')
+    model = torch.load(model_path)
 
     return model
