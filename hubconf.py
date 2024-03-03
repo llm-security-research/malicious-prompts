df754d46993562689a1e7c04019b1bcf7f475599
Hermitao <vkriegergranemann@gmail.com>
2024-03-02 22:32:24 -0300

    Add all models to hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,8 +3,19 @@ import os
 dependencies = ['torch']
 
 def promptsentinel_unbalanced_v1(*args, **kwargs):
-    # model = torch.load("./PromptSentinel/PromptSentinel-Unbalanced-v1/model.pth")
     model_path = os.path.join(os.path.dirname(__file__), 'PromptSentinel/PromptSentinel-Unbalanced-v1/model.pth')
+    model = torch.load(model_path)
+
+    return model
+
+def promptsentinel_balanced_v1(*args, **kwargs):
+    model_path = os.path.join(os.path.dirname(__file__), 'PromptSentinel/PromptSentinel-Balanced-v1/model.pth')
+    model = torch.load(model_path)
+
+    return model
+
+def promptsentinel_unbalanced_paraphrase_v1(*args, **kwargs):
+    model_path = os.path.join(os.path.dirname(__file__), 'PromptSentinel/PromptSentinel-Unbalanced-Paraphrase-v1/model.pth')
     model = torch.load(model_path)
 
     return model
