236664b182a83745550c809f3ae275a031a83cc7
Hermitao <vkriegergranemann@gmail.com>
2024-03-10 21:54:22 -0300

    Add prints to prediction pipeline loading

--- hubconf.py
+++ hubconf.py
@@ -25,11 +25,15 @@ def promptsentinel_unbalanced_paraphrase_v1(*args, **kwargs):
 def prediction_pipeline(*args, **kwargs):
     valid_strings = ['PromptSentinel-Unbalanced-v1', 'PromptSentinel-Balanced-v1', 'PromptSentinel-Unbalanced-Paraphrase-v1']
     arg1 = args[0] if args and args[0] in valid_strings else 'PromptSentinel-Unbalanced-Paraphrase-v1'
+
+    print(f"Loading model {arg1}...")
     model_path = os.path.join(os.path.dirname(__file__), f'PromptSentinel/{arg1}/model.pth')
-
     model = torch.load(model_path) if torch.cuda.is_available() else torch.load(model_path, map_location=torch.device('cpu'))
+    print(f'Model {arg1} loaded successfully.')
 
+    print("Creating prediction pipeline...")
     pipeline = PredictionPipeline(model)
+    print('Pipeline created successfully. Use PredictionPipeline.predict("{text}") to classify prompts.')
 
     return pipeline
 
